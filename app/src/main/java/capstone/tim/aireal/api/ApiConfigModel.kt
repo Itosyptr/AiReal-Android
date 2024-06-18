982906fbad03791473da14b1a6ba5dff675ae010
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2024-06-18 19:53:12 +0800

    fix scrolling bug at home page

--- app/src/main/java/capstone/tim/aireal/api/ApiConfigModel.kt
+++ app/src/main/java/capstone/tim/aireal/api/ApiConfigModel.kt
@@ -4,12 +4,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object ApiConfigModel {
     fun getApiService(): ApiService {
         val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
-
         val client = OkHttpClient.Builder()
+            .callTimeout(0, TimeUnit.MILLISECONDS)
             .addInterceptor(loggingInterceptor)
             .build()
 
