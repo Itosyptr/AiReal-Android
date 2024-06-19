7b1a5dcee61947a977310fd80d9c2f9706ec6edf
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2024-06-20 00:13:21 +0800

    edit order history api

--- app/src/main/java/capstone/tim/aireal/response/UserOrderResponse.kt
+++ app/src/main/java/capstone/tim/aireal/response/UserOrderResponse.kt
@@ -1,44 +1,41 @@
 package capstone.tim.aireal.response
 
+import kotlinx.parcelize.Parcelize
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class UserOrderResponse(
 
-    @field:SerializedName("data")
-    val data: List<OrderItem?>? = null,
+	@field:SerializedName("data")
+	val data: List<OrderItem?>? = null,
 
-    @field:SerializedName("status")
-    val status: String? = null,
-
-    @field:SerializedName("message")
-    val message: String? = null,
+	@field:SerializedName("status")
+	val status: String? = null
 ) : Parcelable
 
 @Parcelize
 data class OrderItem(
 
-    @field:SerializedName("createdAt")
-    val createdAt: CreatedAt? = null,
+	@field:SerializedName("createdAt")
+	val createdAt: CreatedAt? = null,
 
-    @field:SerializedName("id")
-    val id: String? = null,
+	@field:SerializedName("id")
+	val id: String? = null,
 
-    @field:SerializedName("userId")
-    val userId: String? = null,
+	@field:SerializedName("userId")
+	val userId: String? = null,
 
-    @field:SerializedName("items")
-    val items: List<DetailItem?>? = null
+	@field:SerializedName("items")
+	val items: List<ProductOrderItem?>? = null
 ) : Parcelable
 
 @Parcelize
-data class DetailItem(
+data class ProductOrderItem(
 
-    @field:SerializedName("quantity")
-    val quantity: Int? = null,
+	@field:SerializedName("quantity")
+	val quantity: Int? = null,
 
-    @field:SerializedName("productId")
-    val productId: String? = null
+	@field:SerializedName("productId")
+	val productId: String? = null
 ) : Parcelable
